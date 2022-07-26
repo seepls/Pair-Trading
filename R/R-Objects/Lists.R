114c369969a763ac2dd1affbb686d6722c81a89d
Smriti Tiwari <smrititiwari2016@gmail.com>
2022-07-27 02:38:17 +0530

    Delete R/R-Objects directory

--- R/R-Objects/Lists.R
+++
@@ -1,1 +0,0 @@
-# each variable inside the List can have different type, it can even be a list 
--- R/R-Objects/Vector.R
+++
@@ -1,6 +0,0 @@
-# c() : function to make a vector object with more than 1 element 
-apple <-  c('blue','red','green')
-
-# class of the vector 
-print(class(apple))
-
